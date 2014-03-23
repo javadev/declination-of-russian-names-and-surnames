2404f9ba7fde4ca2d328eb89341bf6b125f9040e
javadev <javadev75@gmail.com>
2014-03-23 17:20:55 +0200

    Improved Russian name processor

--- src/main/java/com/github/declinationofnames/RussianNameProcessor.java
+++ src/main/java/com/github/declinationofnames/RussianNameProcessor.java
@@ -18,8 +18,13 @@
 package com.github.declinationofnames;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Declinates of Russian names and surnames.
@@ -48,14 +53,15 @@ public class RussianNameProcessor {
     static String gcasePred = "prepositional";
     static String gcasePos = "prepositional";
     
-    static Map rules = new HashMap() {{
-        put("lastName", new HashMap() {{
-            put("exceptions", new ArrayList() {{
+    static Map<String, Map<String, List<Object>>> rules = new HashMap<String,
+            Map<String, List<Object>>>() {{
+        put("lastName", new HashMap<String, List<Object>>() {{
+            put("exceptions", new ArrayList<Object>() {{
                 add("	дюма,тома,дега,люка,ферма,гамарра,петипа,шандра . . . . .");
                 add("	гусь,ремень,камень,онук,богода,нечипас,долгопалец,маненок,рева,кива . . . . .");
                 add("	вий,сой,цой,хой -я -ю -я -ем -е");
             }});
-            put("suffixes", new ArrayList() {{
+            put("suffixes", new ArrayList<Object>() {{
                 add("f	б,в,г,д,ж,з,й,к,л,м,н,п,р,с,т,ф,х,ц,ч,ш,щ,ъ,ь . . . . .");
                 add("f	ска,цка  -ой -ой -ую -ой -ой");
                 add("f	ая       --ой --ой --ую --ой --ой");
@@ -107,15 +113,15 @@ public class RussianNameProcessor {
                 add("	б,г,д,ж,з,к,л,м,п,р,с,т,ф,х   а у а ом е");                
             }});
         }});
-        put("firstName", new HashMap() {{
-            put("exceptions", new ArrayList() {{
+        put("firstName", new HashMap<String, List<Object>>() {{
+            put("exceptions", new ArrayList<Object>() {{
                 add("	лев    --ьва --ьву --ьва --ьвом --ьве");
                 add("	павел  --ла  --лу  --ла  --лом  --ле");
                 add("m	шота   . . . . .");
                 add("m	пётр   ---етра ---етру ---етра ---етром ---етре");
                 add("f	рашель,нинель,николь,габриэль,даниэль   . . . . .");
             }});
-            put("suffixes", new ArrayList() {{
+            put("suffixes", new ArrayList<Object>() {{
                 add("	е,ё,и,о,у,ы,э,ю   . . . . .");
                 add("f	б,в,г,д,ж,з,й,к,л,м,н,п,р,с,т,ф,х,ц,ч,ш,щ,ъ   . . . . .");
 
@@ -132,16 +138,145 @@ public class RussianNameProcessor {
                 add("	б,в,г,д,ж,з,к,л,м,н,п,р,с,т,ф,х,ц,ч	 а у а ом е");
             }});
         }});
-        put("middleName", new HashMap() {{
-            put("suffixes", new ArrayList() {{
+        put("middleName", new HashMap<String, List<Object>>() {{
+            put("suffixes", new ArrayList<Object>() {{
                 add("	ич   а  у  а  ем  е");
                 add("	на  -ы -е -у -ой -е");
             }});
         }});
     }};
 
+    static {
+        prepareRules();
+    }
+
+    private static void prepareRules() {
+        for (Map.Entry<String, Map<String, List<Object>>> type : rules.entrySet()) {
+            for(Map.Entry<String, List<Object>> key : rules.get(type.getKey()).entrySet()) {
+                for(int i = 0, n = rules.get(type.getKey()).get(key.getKey()).size(); i < n; i++) {
+                    rules.get(type.getKey()).get(key.getKey()).set(i, rule(rules.get(type.getKey()).get(key.getKey())));
+                }
+            }
+        }
+    }
+
+    private static Map<String, List<String>> rule(Object rule) {
+        final Matcher matcher = Pattern.compile(
+                "^\\s*([fm]?)\\s*(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s*$").matcher(rule.toString());
+        if (matcher.matches()) {
+            return new HashMap<String, List<String>>() {{
+                put("sex", Arrays.asList(matcher.group(1)));
+                put("test", Arrays.asList(matcher.group(2).split(",")));
+                put("mods", Arrays.asList(matcher.group(3), matcher.group(4),
+                        matcher.group(5), matcher.group(6), matcher.group(7)));
+            }};
+        }
+        return Collections.<String, List<String>>emptyMap();
+    }
+
+    // склоняем слово по указанному набору правил и исключений
+    private static String word(String word, String sex, String wordType, String gcase) {
+        // исходное слово находится в именительном падеже
+        if (gcase.equals(gcaseNom)) {
+            return word;
+        }
+
+        // составные слова
+        if (word.matches("[-]")) {
+                String[] list = word.split("-");
+                StringBuilder resultList = new StringBuilder();
+                for(int i = 0, n = list.length; i < n; i++) {
+                    if (i > 0) {
+                        resultList.append("-");
+                    }	
+                    resultList.append(word(list[i], sex, wordType, gcase));
+                }
+                return resultList.toString();
+        }
+
+        // Иванов И. И.
+        if (Pattern.compile("^[А-ЯЁ]\\.?$", Pattern.CASE_INSENSITIVE).matcher(word).matches()) {
+            return word;
+        }
+
+        Map<String, List<Object>> localRules = rules.get(wordType);
+
+        if (localRules.get("exceptions") !=  null) {
+                String pick = pick(word, sex, gcase, localRules.get("exceptions"), true);
+                if (pick != null) {
+                    return pick;
+                }
+        }
+        String pick = pick(word, sex, gcase, localRules.get("suffixes"), false);
+        return pick != null ? pick : word;
+    }
+
+    // выбираем из списка правил первое подходящее и применяем 
+    private static String pick(String word, String sex, String gcase, List<Object> rules, boolean matchWholeWord) {
+        String wordLower = word.toLowerCase();
+        for (int i = 0, n = rules.size(); i < n; i++) {
+            if (ruleMatch(wordLower, sex, rules.get(i), matchWholeWord)) {
+                return applyMod(word, gcase, rules.get(i));
+            }
+        }
+        return null;
+    }
+    
+    // проверяем, подходит ли правило к слову
+    private static boolean ruleMatch(String word, String sex, final Object rule, boolean matchWholeWord) {
+        final Map<String, List<String>> localRule = (Map<String, List<String>>) rule;    
+        if (localRule.get("sex").equals(sexM) && sex.equals(sexF)) { 
+                // male by default
+                return false;
+            }
+            if (localRule.get("sex").equals(sexF) && !sex.equals(sexF)) {
+                return false;
+            }
+            for (int i = 0, n = localRule.get("test").size(); i < n; i++) {
+                    String test = matchWholeWord ? word : word.substring(
+                            Math.max(word.length() - localRule.get("test").get(i).length(), 0));
+                    if (test.equals(localRule.get("test").get(i))) {
+                        return true;
+                    }
+            }
+            return false;
+    }
+    
+    // склоняем слово (правим окончание)
+    private static String applyMod(final String word, String gcase, final Object rule) {
+        final Map<String, List<String>> localRule = (Map<String, List<String>>) rule;
+        final String mod;
+        if (gcase.equals(gcaseNom)) {
+            mod = ".";
+        } else if(gcase.equals(gcaseGen)) {
+            mod = localRule.get("mods").get(0);
+        } else if(gcase.equals(gcaseDat)) {
+            mod = localRule.get("mods").get(1);
+        } else if(gcase.equals(gcaseAcc)) {
+            mod = localRule.get("mods").get(2);
+        } else if(gcase.equals(gcaseIns)) {
+            mod = localRule.get("mods").get(3);
+        } else if(gcase.equals(gcasePos)) {
+            mod = localRule.get("mods").get(4);
+        } else {
+            throw new IllegalArgumentException("Unknown grammatic case: " + gcase);
+        }
+        String localWord = word;
+        for(int i = 0, n = mod.length(); i < n; i++) {
+            String c = mod.substring(i, 1);
+            if (".".equals(c)) {
+            } else if ("-".equals(c)) {
+                localWord = localWord.substring(0, localWord.length() - 1);
+            } else {
+                localWord = localWord + c;
+            }
+        }
+        return localWord;
+    }
+
     /**
      */
     public RussianNameProcessor() {
     }
+
 }
